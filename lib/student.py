bb0834612562c721b2d856858cacd6256bf864f8
Mariam Senzia <mariamsenzia@gmail.com>
2024-03-04 15:01:08 +0300

    object inheritance 2

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,14 @@
 class Student:
-    pass
+    def hello(self):
+        return "Hey there! I'm so excited to learn stuff.\n"
+        
+    def raise_hand(self):
+        return 'Pick me!\n'
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        return super().hello() + "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+        
+    def raise_hand(self):
+        return super().raise_hand() * 10
+        
